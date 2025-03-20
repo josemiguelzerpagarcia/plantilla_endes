204598c26efef8745f75d789a91e12f4d8ea38c7
josemiguelzerpagarcia <zerpa.garcia.jose.miguel@iescamas.es>
2025-03-20 10:13:36 +0100

    refactorizacion

--- plantilla/src/test/java/com/endes/entidad/PlantillaTest.java
+++ plantilla/src/test/java/com/endes/entidad/PlantillaTest.java
@@ -21,7 +21,7 @@ class PlantillaTest {
     /**
      * Prueba que verifica que no se puedan contratar empleados con el mismo DNI.
      */
-    @Disabled
+    
     @Test
     @DisplayName("No permite contratar empleados con el mismo DNI")
     void testContratarEmpleado_Duplicado() {
